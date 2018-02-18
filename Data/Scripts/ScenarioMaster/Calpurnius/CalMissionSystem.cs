4711e435242d4d634aa19a80b2df05c5e1c5f5b1
Wicorel <saettler@hotmail.com>
2018-02-18 12:31:27 -0800

    Redo VisStudio Solution to have Master and then Scenario Rename folders (causes big delete/add)

---
+++ Data/Scripts/ScenarioMaster/Calpurnius/CalMissionSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using VRageMath;
+
+namespace Scenario.Calpurnius
+{
+	public class CalMissionSystem : AbstractMissionSystem
+	{
+		// TODO: Need correct waypoints for Calpurnius, below is an example
+		private readonly Vector3D droneWreck = new Vector3D(1854936.75, -2006193.25, 1325297.5);
+
+		private readonly ResearchControl researchControl;
+
+		internal CalMissionSystem(long missionStartTimeBinary, HashSet<int> alreadyExecutedPrompts,
+			QueuedAudioSystem audioSystem, ResearchControl researchControl) : base(missionStartTimeBinary, alreadyExecutedPrompts, audioSystem)
+		{
+			this.researchControl = researchControl;
+		}
+
+		protected override void GeneratePrompts()
+		{
+			// TODO: Add desired tech unlocks for Scenario
+			AddTimePrompt(1000, new TimeSpan(0, 0, 10),
+				UnlockAllTech(),
+				PlayAudioClip(CalAudioClip.AllTechUnlocked));
+
+			AddProximityPrompt(1000, droneWreck, 3000,
+				PlayAudioClip(CalAudioClip.AllTechUnlocked));
+		}
+
+		internal Action UnlockAllTech()
+		{
+			return () =>
+			{
+				foreach (TechGroup techGroup in Enum.GetValues(typeof(TechGroup)))
+				{
+					researchControl.UnlockTechGroupForAllPlayers(techGroup);
+				}
+			};
+		}
+	}
+}
